a8d59c991056323459235bf6b2f5fcd96195c2cd
Moneer Al-Dakkak <77343614+moneerAlDakkak@users.noreply.github.com>
2021-12-13 14:57:57 +0200

    Add files via upload

---
+++ precache-manifest.9b54a4459d704e94b56b3efae655572a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c804ecb551ea99bf44da",
+    "url": "/Textie/css/app.5161828b.css"
+  },
+  {
+    "revision": "79b8bac973df827a0b12",
+    "url": "/Textie/css/chunk-vendors.8f0cb8d6.css"
+  },
+  {
+    "revision": "b30c63d4297993246e499bbeb02a1c2c",
+    "url": "/Textie/index.html"
+  },
+  {
+    "revision": "12d6fad8bfdb155b20ca",
+    "url": "/Textie/js/about.463fe4dc.js"
+  },
+  {
+    "revision": "c804ecb551ea99bf44da",
+    "url": "/Textie/js/app.6abdf600.js"
+  },
+  {
+    "revision": "79b8bac973df827a0b12",
+    "url": "/Textie/js/chunk-vendors.674fb4d8.js"
+  },
+  {
+    "revision": "b549504a34ac8c40b136a97f0d341b13",
+    "url": "/Textie/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Textie/robots.txt"
+  }
+]);
